d8834e0273da9edf07c0983123fbe0166e7b1603
Melody Yu <yu.musicssis@gmail.com>
2023-12-30 20:56:18 +0800

    add landing page

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,5 +1,39 @@
+// eslint-disable-next-line no-unused-vars
+import styled from "styled-components";
+import Wrapper from "../assets/wrappers/LandingPage";
+import main from "../assets/images/main.svg";
+import { Link } from "react-router-dom";
+import { Logo } from "../components";
+
 const Landing = () => {
-  return <div>Landing</div>;
+  return (
+    <Wrapper>
+      <nav>
+        <Logo />
+      </nav>
+      <div className="container page">
+        <div className="info">
+          <h1>
+            job <span>tracking</span>
+          </h1>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Non
+            tempore consectetur modi numquam commodi est maxime iusto et
+            eligendi reiciendis officiis culpa nobis asperiores, sed cumque amet
+            nulla voluptate reprehenderit?
+          </p>
+
+          <Link to="/register" className="btn register-link">
+            Register
+          </Link>
+          <Link to="/login" className="btn ">
+            Login / Demo User
+          </Link>
+        </div>
+        <img src={main} alt="job hunt" className="img main-img" />
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Landing;
