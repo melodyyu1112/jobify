db8e1048147273bfacd496d1ad4885d2ed3f3cf7
Melody Yu <yu.musicssis@gmail.com>
2023-12-30 20:56:48 +0800

    add error page

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,12 +1,27 @@
+/* eslint-disable react/no-unescaped-entities */
 import { Link, useRouteError } from "react-router-dom";
+import Wrapper from "../assets/wrappers/ErrorPage";
+import img from "../assets/images/not-found.svg";
 
 const Error = () => {
   const error = useRouteError();
   console.log(error);
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="not found" />
+          <h3>Ohh! page not found</h3>
+          <p>we can't seem to find the page that you are looking for</p>
+          <Link to="/dashboard">back home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
   return (
     <div>
       <h1> Error Page </h1>
-      <Link to="/" back home></Link>
+      <Link to="/"> back home</Link>
     </div>
   );
 };
